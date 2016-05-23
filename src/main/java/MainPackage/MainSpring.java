4a22af8319c609ee2c3140f3b90140a315d7cccb
Zdyczen <marstyczen@gmail.com>
2016-05-23 13:30:25 +0200

    Simple callendar

--- src/main/java/MainPackage/MainSpring.java
+++ src/main/java/MainPackage/MainSpring.java
@@ -7,17 +7,26 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import sping.user.User;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
 /**
  * Created by stycz0007 on 19.05.16.
  */
 public class MainSpring {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean.xml");
-      //  UserRepo userRepo = applicationContext.getBean("userRepo", UserRepoImplem.class);
-      //  User user =  userRepo.createUser("JanBardzo");
-
-        SomeBean someBean = applicationContext.getBean("SomeBean", SomeBean.class );
-
+//        Locale locale = Locale.getDefault();
+//        Calendar calendar = GregorianCalendar.getInstance(locale);
+//
+//        DateFormat dateFormat = SimpleDateFormat.getDateInstance();
+//        System.out.println(dateFormat.format(calendar.getTime()));
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("config.xml");
+        Calendar calendarBean = applicationContext.getBean("calendar", Calendar.class);
+        DateFormat dateFormat = (DateFormat) applicationContext.getBean("formatter");
+        System.out.println(dateFormat.format(calendarBean.getTime()));
 
     }
 }
